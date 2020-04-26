dcb40190f9025e228f725ac45a7d22b76a62e44b
Iheb23 <64042827+Iheb23@users.noreply.github.com>
2020-04-26 15:41:11 +0100

    Add files via upload

---
+++ build-flying-plane-sdl-animation-Unnamed-Debug/CMakeFiles/MII.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/LENOVO/Documents/mii23-master/flying-plane-sdl-animation/src/animation.c" "C:/Users/LENOVO/Documents/mii23-master/build-flying-plane-sdl-animation-Unnamed-Debug/CMakeFiles/MII.dir/src/animation.c.obj"
+  "C:/Users/LENOVO/Documents/mii23-master/flying-plane-sdl-animation/src/background.c" "C:/Users/LENOVO/Documents/mii23-master/build-flying-plane-sdl-animation-Unnamed-Debug/CMakeFiles/MII.dir/src/background.c.obj"
+  "C:/Users/LENOVO/Documents/mii23-master/flying-plane-sdl-animation/src/enemy.c" "C:/Users/LENOVO/Documents/mii23-master/build-flying-plane-sdl-animation-Unnamed-Debug/CMakeFiles/MII.dir/src/enemy.c.obj"
+  "C:/Users/LENOVO/Documents/mii23-master/flying-plane-sdl-animation/src/image.c" "C:/Users/LENOVO/Documents/mii23-master/build-flying-plane-sdl-animation-Unnamed-Debug/CMakeFiles/MII.dir/src/image.c.obj"
+  "C:/Users/LENOVO/Documents/mii23-master/flying-plane-sdl-animation/src/main.c" "C:/Users/LENOVO/Documents/mii23-master/build-flying-plane-sdl-animation-Unnamed-Debug/CMakeFiles/MII.dir/src/main.c.obj"
+  "C:/Users/LENOVO/Documents/mii23-master/flying-plane-sdl-animation/src/plane.c" "C:/Users/LENOVO/Documents/mii23-master/build-flying-plane-sdl-animation-Unnamed-Debug/CMakeFiles/MII.dir/src/plane.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "C:/Users/LENOVO/Documents/mii23-master/flying-plane-sdl-animation/include"
+  "C:/SDL2-2.0.10/i686-w64-mingw32/include/SDL2"
+  "C:/SDL2_image-2.0.5/i686-w64-mingw32/include/SDL2"
+  "C:/SDL2_ttf-2.0.15/i686-w64-mingw32/include/SDL2"
+  "C:/SDL2_mixer-2.0.4/i686-w64-mingw32/include/SDL2"
+  "C:/SDL2_gfx-1.0.4/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build-flying-plane-sdl-animation-Unnamed-Debug/CMakeFiles/MII.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MII.dir/src/animation.c.obj"
+  "CMakeFiles/MII.dir/src/background.c.obj"
+  "CMakeFiles/MII.dir/src/enemy.c.obj"
+  "CMakeFiles/MII.dir/src/image.c.obj"
+  "CMakeFiles/MII.dir/src/main.c.obj"
+  "CMakeFiles/MII.dir/src/plane.c.obj"
+  "MII.exe"
+  "MII.exe.manifest"
+  "MII.pdb"
+  "libMII.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MII.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
